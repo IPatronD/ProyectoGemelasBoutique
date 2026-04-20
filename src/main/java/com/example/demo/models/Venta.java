78959ce6a3930e9c023ac98711c0db46798202e6
CristhianVP03 <cristhian_alexander28@hotmail.com>
2026-04-20 10:24:56 -0500

    Progreso Modelo Venta

---
+++ src/main/java/com/example/demo/models/Venta.java
@@ -0,0 +1,36 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ventas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+
+    @NotNull
+    private double total;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+}
