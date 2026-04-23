eb52ddfaf21ecfa6511df5deb96399dae67ba3bd
IPatronD <dcll.081105@gmail.com>
2026-04-23 01:16:01 -0500

    Servicios Usuarios y Empleados

--- src/main/java/com/example/demo/service/EmpleadoService.java
+++ src/main/java/com/example/demo/service/EmpleadoService.java
@@ -1,30 +1,4 @@
 package com.example.demo.service;
 
-import com.example.demo.models.Empleado;
-import com.example.demo.repository.EmpleadoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class EmpleadoService {
-    @Autowired
-    private EmpleadoRepository repository;
-
-    public List<Empleado> listar() {
-        return repository.findAll();
-    }
-
-    public Empleado guardar(Empleado empleado) {
-        return repository.save(empleado);
-    }
-
-    public Empleado obtener(Long id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    public void eliminar(Long id) {
-        repository.deleteById(id);
-    }
+public interface EmpleadoService {
 }
