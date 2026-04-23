5f782f771b68094663eb1103c4f5dde09ea07c77
CristhianVP03 <cristhian_alexander28@hotmail.com>
2026-04-23 12:34:39 -0500

    Ajustes

--- src/main/java/com/example/demo/models/Venta.java
+++ src/main/java/com/example/demo/models/Venta.java
@@ -2,16 +2,13 @@ package com.example.demo.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ventas")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Venta {
 
     @Id
