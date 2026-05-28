ed724c9a0c6891b18d2cce47fa1b07874f3c0f4e
SebastianHDC <alitriji.xyz@gmail.com>
2026-05-27 23:21:54 -0500

    Update DemoApplication.java
    Agregando la contraseña principal, para que se conecte con la cifrada, haciendo comparaciones, si es correcta.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
-//holaaaa
+	// holaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner testPassword(
+			BCryptPasswordEncoder encoder) {
+
+		return args -> {
+			System.out.println(
+					encoder.encode("123456"));
+		};
+	}
 }
+
